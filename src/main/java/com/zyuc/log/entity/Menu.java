8a72769a3f5c7949e9f4b0680ea4c2abdff56c4c
hongwj <2233128678@qq.com>
2021-01-06 10:10:26 +0800

    菜单树生成

--- src/main/java/com/zyuc/log/entity/Menu.java
+++ src/main/java/com/zyuc/log/entity/Menu.java
@@ -1,5 +1,9 @@
 package com.zyuc.log.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import lombok.Data;
 
@@ -11,17 +15,21 @@ import java.util.Date;
  * @date 2021/01/06
  **/
 @Data
+@TableName("sys_log")
 public class Menu extends Model<Menu> implements Serializable {
+
     private static final long serialVersionUID = 1L;
 
     /**
      * 主键
      */
+    @TableId(type = IdType.AUTO, value = "menu_id")
     private Long menuId;
 
     /**
      * 父菜单ID，一级菜单为0
      */
+    @TableField("parent_id")
     private Long parentId;
 
     /**
@@ -31,12 +39,10 @@ public class Menu extends Model<Menu> implements Serializable {
     /**
      * 菜单URL
      */
-
     private String url;
     /**
      * 授权(多个用逗号分隔，如：user:list,user:create)
      */
-
     private String perms;
     /**
      * 类型 0：目录 1：菜单 2：按钮
@@ -49,14 +55,17 @@ public class Menu extends Model<Menu> implements Serializable {
     /**
      * 排序
      */
+    @TableField("order_num")
     private Integer orderNum;
     /**
      * 创建时间
      */
+    @TableField("gmt_create")
     private Date gmtCreate;
     /**
      * 修改时间
      */
+    @TableField("gmt_modified")
     private Date gmtModified;
 
 }
